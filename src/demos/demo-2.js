d7efdfb003d4c04aab1d8c5795ff6a548a786e5b
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2020-07-30 19:14:16 +0800

    Demo 2 reset hover

--- src/demos/demo-2.js
+++ src/demos/demo-2.js
@@ -3,8 +3,10 @@ import tween from "../galaw/tween";
 import spring from "../galaw/spring";
 import autoplay from "../galaw/autoplay";
 import fromMouseMove from "../galaw/fromMouseMove";
-import { noop } from "../galaw/utils";
+import { noop, subscribe } from "../galaw/utils";
 import { createSpringSettings } from "./utils";
+import fromEvent from "../galaw/fromEvent";
+import throttleRAF from "../galaw/throttleRAF";
 
 const container = document.getElementById('demo-2');
 const playground = container.getElementsByClassName('playground')[0]
@@ -26,35 +28,58 @@ const bouncy = (displacement = 100) =>
     map(data => Math.abs(data.displacement - displacement) / displacement)
   );
 
+
+const bounce = (from, to) => {
+  const { x: fromX, y: fromY } = from;
+  const { x: toX, y: toY } = to;
+  return autoplay(
+    pipe(
+      bouncy(),
+      collectLatest([
+        tween(p => p(fromX, toX)),
+        tween(p => p(fromY, toY)),
+      ])
+    )
+  );
+}
+
 const center = { x: playground.clientWidth / 2, y: playground.clientHeight / 2 };
 const { top, left } = playground.getBoundingClientRect();
 const lastCenterRatio = { x: 0, y: 0 };
 
+const draw = ([x, y]) => {
+  lastCenterRatio.x = x;
+  lastCenterRatio.y = y;
+
+  const xr = x / center.x;
+  const yr = y / center.y;
+
+  const translate = `translate(${xr * 25}px, ${yr * 25}px)`;
+  const rotateX = `rotateX(${-(yr * 25)}deg)`;
+  const rotateY = `rotateY(${xr * 25}deg)`;
+  const scale = `scale(${(.5 * Math.abs(xr)) + 1})`;
+  const perspective = `perspective(600px)`;
+
+  square.style.transform = `
+    ${perspective}
+    ${translate}
+    ${rotateX}
+    ${rotateY}
+    ${scale}
+  `;
+}
+
 pipe(
   fromMouseMove(playground),
+  tap(() => console.log('move')),
   map(e => ({ x: e.clientX - left - center.x, y: e.clientY - top - center.y })),
-  switchMap(({ x, y }) => {
+  switchMap(({ x, y }) => bounce({ ...lastCenterRatio }, { x, y })),
+  subscribe(draw)
+)
 
-    const lastCenterRatioX = lastCenterRatio.x;
-    const lastCenterRatioY = lastCenterRatio.y;
-    return autoplay(
-      pipe(
-        bouncy(),
-        collectLatest([
-          tween(p => p(lastCenterRatioX, x)),
-          tween(p => p(lastCenterRatioY, y)),
-        ]),
-        tap(([xr, yr]) => {
-          lastCenterRatio.x = xr;
-          lastCenterRatio.y = yr;
-        })
-      )
-    );
-  }),
-  map(([xr, yr]) => [(xr / center.x), (yr / center.y)]),
-  map(([xr, yr]) => ({ transform: `perspective(600px) translate(${xr * 25}px, ${yr * 25}px)  rotateX(${-(yr * 25)}deg) rotateY(${xr * 25}deg) scale(${(.5 * Math.abs(xr)) + 1})` })),
-  listen(
-    noop,
-    style => Object.assign(square.style, style)
-  )
+pipe(
+  fromEvent(playground, 'mouseleave'),
+  throttleRAF,
+  switchMap(() => bounce({ ...lastCenterRatio }, { x: 0, y: 0 })),
+  subscribe(draw)
 )
